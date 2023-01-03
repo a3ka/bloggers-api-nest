39a1793d1109ceaedbb6ebf22e5d01073b52904a
aka <alex.kurz@protonmail.com>
2023-01-03 13:58:34 +0100

    vercel#4

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { runDb } from './db';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './http-exception.filter';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   // await runDb();
